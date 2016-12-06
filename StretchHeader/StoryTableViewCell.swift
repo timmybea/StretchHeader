836c120b01a0f21983291cddbaf89587a5464c1a
Tim Beals <tim.beals@gmail.com>
2016-12-06 11:41:48 -0800

    basic table setup

---
+++ StretchHeader/StoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StoryTableViewCell.swift
+//  StretchHeader
+//
+//  Created by Tim Beals on 2016-12-06.
+//  Copyright © 2016 Tim Beals. All rights reserved.
+//
+
+import UIKit
+
+class StoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ StretchHeader/TableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewController.swift
+//  StretchHeader
+//
+//  Created by Tim Beals on 2016-12-06.
+//  Copyright © 2016 Tim Beals. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
