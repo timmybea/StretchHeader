cbf3ea45a0311d47fe991416c9c08165ec368c54
Tim Beals <tim.beals@gmail.com>
2016-12-06 17:01:32 -0800

    completed stretch header

--- StretchHeader/TableViewController.swift
+++ StretchHeader/TableViewController.swift
@@ -8,28 +8,58 @@
 
 import UIKit
 
-class TableViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+class TableViewController: UIViewController,UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var dateLabel: UILabel!
+    private let kTableHeaderHeight: CGFloat = 250
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+        tableView.delegate = self
+//        tableView.estimatedRowHeight = 130
+////        
+//        tableView.rowHeight = UITableViewAutomaticDimension
+////        
+        //Set date above the table view
+        let dateformatter = DateFormatter()
+        dateformatter.dateStyle = .full
+        let now = dateformatter.string(from: NSDate() as Date)
+        self.dateLabel.text = now
         
-        tableView.estimatedRowHeight = 70
-        tableView.rowHeight = UITableViewAutomaticDimension
+        //remove the header view from the table view
+        headerView = tableView.tableHeaderView
+        tableView.tableHeaderView = nil
+        //add the header view back into the view heirarchy
+        tableView.addSubview(headerView)
         
+        tableView.contentInset = UIEdgeInsetsMake(kTableHeaderHeight, 0, 0, 0)
+        tableView.contentOffset = CGPoint(x: 0, y: -kTableHeaderHeight)
         
-        
+        tableView.reloadData()
+        updateHeaderView()
     }
     
     //MARK: tableview delegate methods
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "StoryTableViewCell") as! StoryTableViewCell
-        
+        cell.newsItem = items[indexPath.row]
         return cell
     }
     
@@ -38,8 +68,29 @@ class TableViewController: UIViewController,UITableViewDelegate, UITableViewData
         return true
     }
     
-    //MARK: data setup
+    //MARK: Header View
     
+    @IBOutlet weak var headerView: UIView!
+//    var headerView: UIView!
+    
+    func updateHeaderView()
+    {
+        var headerRect = CGRect(x: 0, y: -kTableHeaderHeight, width: tableView.bounds.width, height: kTableHeaderHeight)
+        if tableView.contentOffset.y < -kTableHeaderHeight
+        {
+            headerRect.origin.y = tableView.contentOffset.y
+            headerRect.size.height = -tableView.contentOffset.y
+        }
+        headerView.frame = headerRect
+    }
+  
+    //MARK: Scroll view
+     func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        updateHeaderView()
+    }
+    
+    
+    //MARK: data setup
     let items = [
         NewsItem(category: .World, summary: "Climate change protests, divestments meet fossil fuel realities"),
         NewsItem(category: .Europe, summary: "Scotland's 'Yes' leader says independence vote is once in a lifetime"),
@@ -51,16 +102,5 @@ class TableViewController: UIViewController,UITableViewDelegate, UITableViewData
         NewsItem(category: .Europe, summary: "One million babies' created by EU exchange students")
         ]
 
-        
-
-
-
-
-        
-
-
-    
-    
-    ]
     
 }
